b3dc5476aa7fcd773873906d47b9aeea3ba14c47
Francesco Docchi <45453358+frankie-mceyes@users.noreply.github.com>
2019-06-19 01:46:17 +0200

    File validator and check timeout
    [+] Added file validator. Now if file contains spaces, the script aborts
    [+] Added check for timeout input. Now if is not an integer, the script aborts

--- spidervacuum.sh
+++ spidervacuum.sh
@@ -6,20 +6,24 @@ echo "\ \`--. _ __  _  __| | ___ _ __| | | | __ _  ___ _   _ _   _ _ __ ___  ";
 echo " \`--. \ '_ \| |/ _\` |/ _ \ '__| | | |/ _\` |/ __| | | | | | | '_ \` _ \ ";
 echo "/\__/ / |_) | | (_| |  __/ |  \ \_/ / (_| | (__| |_| | |_| | | | | | |";
 echo "\____/| .__/|_|\__,_|\___|_|   \___/ \__,_|\___|\__,_|\__,_|_| |_| |_|";
-echo "    | |";
-echo "    |_|";
+echo "	  | |";
+echo "	  |_|";
 echo ""
 
 echo -n 'Dictionary file path: '
 read dict
 
-if [ -z "$dict" ]																							   ### If variable is empty or file doesn't exist
+if [ -z "$dict" ]															### If variable is empty or file doesn't exist
 then
-		echo Missing dictionary! You must specify a dictionary of directories.
+		echo 'Missing dictionary! You must specify a dictionary of directories.'
 		exit 1
 elif [ ! -f "$dict" ]
 then
-		echo File not found. Are you sure you typed the right path?
+		echo 'File not found. Are you sure you typed the right path?'
+		exit 1
+elif grep -q ' ' $dict
+then
+		echo 'File contains spaces. URLs should not contain spaces. Please, check your dictionary.'				### If file contains spaces
 		exit 1
 fi
 
@@ -27,11 +31,11 @@ echo -n 'URL: '
 read url
 
 echo -ne '\tChecking if URL is alive... '
-if [ -z "$url" ]																								### If variable is empty
+if [ -z "$url" ]															### If variable is empty
 then
-		echo Missing URL to scan!
+		echo 'Missing URL to scan!'
 		exit 1
-elif [[ "$(ping -c 1 -w 3 $url > /dev/null 2>&1; echo $?)" != 0 ]]											  ### If exit code of ping to URL is not 0, means that URL is unreachable
+elif [[ "$(host $url > /dev/null 2>&1; echo $?)" != 0 ]]										### If exit code of host command to URL is not 0, means that URL is unreachable
 then
 		echo 'URL non reachable, aborting.'
 		exit 1
@@ -42,27 +46,31 @@ fi
 echo -n 'Timeout: '
 read tout
 
-if [ -z "$tout" ]																							   ### If timeout is not specified, set the default of 3
+if [ -z "$tout" ]															### If timeout is not specified, set the default of 3
 then
-		echo -e '\tTimeout not specified. Using the default 3 seconds'
+		echo -e '\tTimeout not specified. Using the default 3 seconds.'
 		tout=3
+elif ! [ "$tout" -eq "$tout" ] 2>/dev/null												### If value inserted is an integer number
+then
+		echo -e "Value not valid. Write the seconds you want to wait for a response."
+		exit 1
 fi
 
 echo -n 'Stealth: '
 read stealth
 
-if [ -z "$stealth" ]																							### If stealth delay is not specified
+	if [ -z "$stealth" ]														### If stealth delay is not specified
 then
-		echo -e "\tStealth not specified. Every packet will be sent ASAP"
+		echo -e "\tStealth not specified. Every packet will be sent ASAP."
 		stealth=0
-elif ! [ "$stealth" -eq "$stealth" ] 2>/dev/null																### If value inserted is an integer number
+elif ! [ "$stealth" -eq "$stealth" ] 2>/dev/null											### If value inserted is an integer number
 then
-		echo -e "Value not valid. Write the seconds you want to wait for each request to be delivered"
+		echo -e "Value not valid. Write the seconds you want to wait for each request to be delivered."
 		exit 1
 fi
 
-countdown=$(wc -l $dict | awk '{print$1}')																	  ### Initialize total count of pending URL
-count=1																										 ### Initialize counter for URL countdown
+		countdown=$(wc -l $dict | awk '{print$1}')										### Initialize total count of pending URL
+count=1																	### Initialize counter for URL countdown
 
 echo -e '\nScanning URL '"$url"' with dictionary located in '"$dict"'. Total URLs pending: '"$countdown"
 
@@ -83,4 +91,4 @@ echo
 
 
 ### For any clarification about HTTP return codes, check here:
-### 	https://thevirusdoublezero.tk/codici-http/
+###	 https://thevirusdoublezero.tk/codici-http/
