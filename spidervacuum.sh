3bdd12c3fb0db210f602081b229d3c899023c66d
Francesco Docchi <45453358+frankie-mceyes@users.noreply.github.com>
2019-06-18 22:07:02 +0200

    Added URL check, countdown and formatted outputs
    [+] Host is checked after user writes it, so if is offline, the script won't go further
    [+] Added countdown at the end of the lines and the first output, so user knows how many URL are checked and how many are pending
    [+] Added more HTTP status code to filter (some specific 400-499 codes are theoretically errors, but they mean that the resource the user is looking for does exist in the server)
    
    [~] Added comments (other devs love comments) so code is more readable to non-devs
    [~] Output is more formatted with tabs, to separate sub tasks (checks) from main tasks (inputs)

--- spidervacuum.sh
+++ spidervacuum.sh
@@ -1,79 +1,86 @@
 #!/bin/bash
 
-echo " _____       _     _           _   _                                  ";
-echo "/  ___|     (_)   | |         | | | |                                 ";
+echo " _____	   _	 _		   _   _";
+echo "/  ___|	 (_)   | |		 | | | |";
 echo "\ \`--. _ __  _  __| | ___ _ __| | | | __ _  ___ _   _ _   _ _ __ ___  ";
 echo " \`--. \ '_ \| |/ _\` |/ _ \ '__| | | |/ _\` |/ __| | | | | | | '_ \` _ \ ";
 echo "/\__/ / |_) | | (_| |  __/ |  \ \_/ / (_| | (__| |_| | |_| | | | | | |";
 echo "\____/| .__/|_|\__,_|\___|_|   \___/ \__,_|\___|\__,_|\__,_|_| |_| |_|";
-echo "      | |                                                             ";
-echo "      |_|                                                             ";
+echo "    | |";
+echo "    |_|";
+echo ""
 
-echo -ne 'Dictionary file path: '
+echo -n 'Dictionary file path: '
 read dict
 
-### If variable is empty or file doesn't exist
-if [ -z "$dict" ]
+if [ -z "$dict" ]																							   ### If variable is empty or file doesn't exist
 then
-	echo Missing dictionary! You must specify a dictionary of directories.
-	exit 1
+		echo Missing dictionary! You must specify a dictionary of directories.
+		exit 1
 elif [ ! -f "$dict" ]
 then
-	echo File not found. Are you sure you typed the right path?
-	exit 1
+		echo File not found. Are you sure you typed the right path?
+		exit 1
 fi
 
-echo -ne 'URL: '
+echo -n 'URL: '
 read url
 
-### If variable is empty
-if [ -z "$url" ]
+echo -ne '\tChecking if URL is alive... '
+if [ -z "$url" ]																								### If variable is empty
 then
-	echo Missing URL to scan!
-	exit 1
+		echo Missing URL to scan!
+		exit 1
+elif [[ "$(ping -c 1 -w 3 $url > /dev/null 2>&1; echo $?)" != 0 ]]											  ### If exit code of ping to URL is not 0, means that URL is unreachable
+then
+		echo 'URL non reachable, aborting.'
+		exit 1
+else
+		echo -ne 'OK\n'
 fi
 
-echo -ne 'Timeout: '
+echo -n 'Timeout: '
 read tout
 
-### If timeout is not specified, set the default of 3 seconds
-if [ -z "$tout" ]
+if [ -z "$tout" ]																							   ### If timeout is not specified, set the default of 3
 then
-	echo -e 'Timeout not specified. Using the default 3 seconds'
-	tout=3
+		echo -e '\tTimeout not specified. Using the default 3 seconds'
+		tout=3
 fi
 
-echo -ne 'Stealth: '
+echo -n 'Stealth: '
 read stealth
 
-### If the string inside $stealth is empty, reject it
-if [ -z "$stealth" ]
+if [ -z "$stealth" ]																							### If stealth delay is not specified
 then
-	echo -e "Stealth not specified. Every packet will be sent ASAP"
-	stealth=0
-### If the string inside $stealth is not a number, reject it
-elif ! [ "$stealth" -eq "$stealth" ] 2>/dev/null	
+		echo -e "\tStealth not specified. Every packet will be sent ASAP"
+		stealth=0
+elif ! [ "$stealth" -eq "$stealth" ] 2>/dev/null																### If value inserted is an integer number
 then
-	echo -e "Value not valid. Write the seconds you want to wait for each request to be delivered"
-	exit 1
+		echo -e "Value not valid. Write the seconds you want to wait for each request to be delivered"
+		exit 1
 fi
 
-echo -e '\nScanning URL '"$url"' with dictionary located in '"$dict"'...'
+countdown=$(wc -l $dict | awk '{print$1}')																	  ### Initialize total count of pending URL
+count=1																										 ### Initialize counter for URL countdown
 
-### For every line in dictionary file
+echo -e '\nScanning URL '"$url"' with dictionary located in '"$dict"'. Total URLs pending: '"$countdown"
+
 for crawl in $(cat "$dict")
-	do
-		### Get just the HTTP EXIT CODE with the desided timeout, without showing anything to output
-		EXIT_CODE=$(curl -sL -w '%{http_code}' --connect-timeout $tout -o /dev/null $url/$crawl)
-		if [[ $EXIT_CODE = 20* ]] || [[ $EXIT_CODE = 30* ]]
-		then
-			### If the HTTP exit code is equal to anything between 200 and 299 (HTTP success) or 300-399 (HTTP redirect), take that as valid and print it, creating then a new line
-			echo -ne "\033[2K\u2713 $url/$crawl, HTTP $EXIT_CODE\n"
-		else
-			### If the HTTP exit code is anything else than 20* or 30*, delete the line, print the URL and return to the back of the line
-			echo -ne "\033[2K\u2610 $url/$crawl, HTTP $EXIT_CODE\r"
-		fi
-		### If specified, sleep the amount of time after sending the packet, in order to avoid troubles wihth IDS or IPS
-		sleep $stealth
-	done
+		do
+				EXIT_CODE=$(curl -sL -w '%{http_code}' --connect-timeout $tout -o /dev/null $url/$crawl)		### Just a check of the HTTP return code
+				### The following codes are the 200-299 (success), the 300-399 (redirect) and specific 400-499 codes that, even if they are theorically errors, mean that the resource we are looking for does exist in the server
+				if [[ $EXIT_CODE = 20* ]] || [[ $EXIT_CODE = 30* ]] || [[ $EXIT_CODE = 401 ]] || [[ $EXIT_CODE = 403 ]] || [[ $EXIT_CODE = 407 ]] || [[ $EXIT_CODE = 407 ]] || [[ $EXIT_CODE = 423 ]] || [[ $EXIT_CODE = 451 ]]
+				then
+						echo -ne "\033[2K\u2713 $url/$crawl, HTTP $EXIT_CODE; URL $count of $countdown\n"
+				else
+						echo -ne "\033[2K\u2610 $url/$crawl, HTTP $EXIT_CODE; URL $count of $countdown\r"
+				fi
+				sleep $stealth
+				count=$((count+1))
+		done
 echo
+
+
+### For any clarification about HTTP return codes, check here:
+### 	https://thevirusdoublezero.tk/codici-http/
